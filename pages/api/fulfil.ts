1c9bd6ee03b30148065aa478ee469ad0d2ca442c
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2024-12-14 16:54:39 +0800

    Added API for indicating order fulfilment

---
+++ pages/api/fulfil.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/app/lib/prisma-client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse) {
+    // Query inventory with optional search params
+    if (req.method === 'POST') {
+      const order : { id: string } = JSON.parse(req.body);
+      await prisma.cart.update({
+        where: {
+          id: order.id
+        },
+        data: {
+          fulfilled: true
+        }
+      })
+      .then((_data: any) => {
+        res.status(201).json({ message: 'Order fulfilled.', details: '' });
+      })
+      .catch((_err: any) => {
+        res.status(400).json({
+          message: 'Order fulfil error.',
+          details: 'Order may not exist, or could be an internal error.'
+        });
+      });
+    } else {
+      res.status(400).json({ message: 'Method not available.', details: '' });
+    }
+}
